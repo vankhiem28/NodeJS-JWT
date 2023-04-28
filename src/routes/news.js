c332b41b90ec69ab7d16c385945febec0bdbc5f6
Vankhiem <vankhiem@gmail.com>
2023-04-28 10:35:34 +0700

    Add .gitignore file

--- src/routes/news.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import newsController from "../controllers/NewsController.js";
-const router = express.Router();
-
-router.get("/:slug", newsController.show);
-router.get("/", newsController.index);
-
-export default router;
--- src/routes/site.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import SiteController from "../controllers/SiteController.js";
-const router = express.Router();
-
-router.get("/search", SiteController.search);
-router.get("/", SiteController.home);
-
-export default router;
